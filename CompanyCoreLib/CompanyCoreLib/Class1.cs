5daba6858ca2b6f156e358b37b6d8a086bde5c1d
yurareshetov <64086786+yurareshetov@users.noreply.github.com>
2021-06-01 09:24:21 +0300

    tests

---
+++ CompanyCoreLib/CompanyCoreLib/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyCoreLib
+{
+    public class Analytics
+    {
+
+        public Boolean CheckPrice(int price)
+        {
+            if (price <= 100)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+
+        public int DiscountPrice(int price)
+        {
+            int sum = ((price / 100) * 15) * price;
+            return sum;
+        }
+
+        public string PriceNotNull(int price)
+        {
+            if (price <= 0)
+            {
+                return "Цена не может быть меньше или равна нулю!!!";
+            }
+            else
+                return "Всё правильно";
+        }
+    }
+}
---
+++ CompanyCoreLib/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using CompanyCoreLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestProject1
+    {
+        static Analytics disk_price;
+        [ClassInitialize]
+        static public void Init(TestContext tc)
+        {
+            disk_price = new Analytics();
+        }
+
+
+        //Проверка на размер цены
+        [TestMethod]
+        public void CheckPrice()
+        {
+            Assert.IsTrue(disk_price.CheckPrice(5000));
+        }
+
+
+
+
+
+        //Проверка на тип данных
+        [TestMethod]
+        public void ValidationPrice()
+        {
+            Assert.IsInstanceOfType(disk_price.DiscountPrice(100), typeof(int));
+        }
+
+        //Проверка на нулевую цену
+        [TestMethod]
+        public void PriceNotNull()
+        {
+            Assert.AreEqual(disk_price.PriceNotNull(-12), "Цена не может быть меньше или равна нулю!!!");
+        }
+    }
+}
